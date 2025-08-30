407e9b1213e031f98ce46a2874c3498ae87d89f3
Saiansh <saianshkhanna@gmail.com>
2025-08-30 23:48:37 +0530

    Added file Task2 - Medium

---
+++ Task2 - Medium.py
@@ -0,0 +1,49 @@
+def main(): # Creating a function to perform
+    expenses = {}  # Dictionary to store category input by user
+    total = 0
+
+    while True:
+        print("\n|| ~ Personal Expense Tracker ~ ||")
+        print("1. Add Expense")
+        print("2. View Total Spent")
+        print("3. View Per-Category Breakdown")
+        print("4. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            try:
+                amount = float(input("Enter amount spent: "))
+                category = input("Enter category (e.g. Food, Travel, Shopping): ")
+
+                # Adds amount to total
+                total += amount
+
+                # Adds to category
+                if category in expenses:
+                    expenses[category] += amount
+                else:
+                    expenses[category] = amount
+
+                print(f"Added {amount} in {category}.")
+            except ValueError:
+                print("Invalid amount! Please enter a number.")
+
+        elif choice == "2":
+            print(f"\nTotal Spent: {total}")
+
+        elif choice == "3":
+            if not expenses:
+                print("\nNo expenses recorded yet.")
+            else:
+                print("\nPer-Category Breakdown:")
+                for cat, amt in expenses.items():
+                    print(f"   {cat}: {amt}")
+
+        elif choice == "4":
+            print("Exiting, Now... Goodbye!\nThank You!")
+            break
+        else:
+            print("Invalid choice, Please try again.")
+
+main()
